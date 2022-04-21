2a87e5f6b7f7ed6c90c087c4167ecbc67899137f
Clay Risser <jamrizzi@gmail.com>
2022-04-21 09:26:45 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,4 @@
-var express = require('express');
-var app = express();
 var port = 3000;
+var app = require('express')();
 app.use(express.static('public'));
-app.listen(port, function() {
-    console.log('Server listening on port ' + port);
-});
+app.listen(port, () => console.log('listening on port ' + port));
